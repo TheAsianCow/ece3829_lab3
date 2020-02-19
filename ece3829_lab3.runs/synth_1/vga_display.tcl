e3476fac0acbb45e280e0c5af85b50753f28473f
Alyssa Ungerer <alyssaungerer@gmail.com>
2020-02-18 20:12:20 -0500

    debouncer doesnt work and im stressed....will cry soon

--- ece3829_lab3.runs/synth_1/vga_display.tcl
+++ ece3829_lab3.runs/synth_1/vga_display.tcl
@@ -17,24 +17,26 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param chipscope.maxJobs 1
+set_param xicom.use_bs_reader 1
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.cache/wt} [current_project]
-set_property parent.project_path {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.xpr} [current_project]
+set_property webtalk.parent_dir C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.cache/wt [current_project]
+set_property parent.project_path C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo {c:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.cache/ip} [current_project]
+set_property ip_output_repo c:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib {
-  {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/sources_1/imports/Downloads/Debounce.v}
-  {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/slowclk_1M.v}
-  {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/vga_clk.v}
-  {C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/vga_display.v}
+  C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/sources_1/imports/Downloads/Debounce.v
+  C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/slowclk_1M.v
+  C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/vga_clk.v
+  C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/sources_1/new/vga_display.v
 }
-read_vhdl -library xil_defaultlib {{C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/sources_1/imports/ECE3829/vga_controller_640_60.vhd}}
+read_vhdl -library xil_defaultlib C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/sources_1/imports/ECE3829/vga_controller_640_60.vhd
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
@@ -43,8 +45,8 @@ read_vhdl -library xil_defaultlib {{C:/Users/Jeffrey Huang/Documents/WPI/2019-20
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc {{C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/constrs_1/new/lab3_constraints.xdc}}
-set_property used_in_implementation false [get_files {{C:/Users/Jeffrey Huang/Documents/WPI/2019-2020/C term/ECE3829/ece3829_lab3/ece3829_lab3.srcs/constrs_1/new/lab3_constraints.xdc}}]
+read_xdc C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/constrs_1/new/lab3_constraints.xdc
+set_property used_in_implementation false [get_files C:/Users/alyss/Documents/GitHub/ece3829_lab3/ece3829_lab3.srcs/constrs_1/new/lab3_constraints.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
